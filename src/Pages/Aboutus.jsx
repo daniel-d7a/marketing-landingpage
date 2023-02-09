e6e6dadfe201c61987d1b95357af53e2c6346517
daniel-d7a <eyad.taym.saba@gmail.com>
2023-02-09 23:05:32 +0200

    added rings

--- src/Pages/Aboutus.jsx
+++ src/Pages/Aboutus.jsx
@@ -1,21 +1,23 @@
-import { useRef } from "react";
+import { useRef, useState } from "react";
 import img1 from "../assets/SVG/Scenes/business, statistics _ presentation, project, chart, graph, performance, analytics, man, people.svg";
 
 export default function About() {
   const carousel = useRef(null);
 
   function scrollImageIntoView(index) {
-    console.log(index);
     const img = carousel.current.querySelectorAll(":scope > div")[index];
-    img.scrollIntoView({ behavior: "smooth" });
+    img.scrollIntoView({ behavior: "smooth", block: "end", inline: "nearest" });
   }
+
+  const outline = "ring-2 ring-offset-1	ring-orange-500";
+  const [styles, setStyles] = useState(["", "", ""]);
 
   return (
     <>
       <section
         id="home"
         ref={carousel}
-        className="w-full md:h-[50vh] pt-10 flex md:overflow-x-hidden overflow-auto snap-x"
+        className="w-full md:h-[50vh] pt-10 flex md:overflow-y-hidden overflow-auto snap-x"
       >
         <div className="p-6 flex-shrink-0 w-[100vw] snap-start md:flex md:flex-row-reverse justify-center items-center md:px-10">
           <img src={img1} className="w-2/3 m-auto md:w-1/3" />
@@ -68,21 +70,24 @@ export default function About() {
       </section>
       <div className="flex gap-3 justify-center items-center mt-3">
         <div
-          className="w-3 h-3 rounded-full bg-orange-500"
+          className={`w-3 h-3 rounded-full bg-orange-500 ${styles[0]}`}
           onClick={() => {
             scrollImageIntoView(0);
+            setStyles([outline, "", ""]);
           }}
         ></div>
         <div
-          className="w-3 h-3 rounded-full bg-orange-500"
+          className={`w-3 h-3 rounded-full bg-orange-500 ${styles[1]} `}
           onClick={() => {
             scrollImageIntoView(1);
+            setStyles(["", outline, ""]);
           }}
         ></div>
         <div
-          className="w-3 h-3 rounded-full bg-orange-500"
+          className={`w-3 h-3 rounded-full bg-orange-500 ${styles[2]}`}
           onClick={() => {
             scrollImageIntoView(2);
+            setStyles(["", "", outline]);
           }}
         ></div>
       </div>
